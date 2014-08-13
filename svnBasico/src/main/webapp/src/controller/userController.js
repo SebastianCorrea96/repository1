359b9e377ed481819147aa4c212fc31050aa8d4f
SebastianCorrea96 <s.siza10@uniandes.edu.co>
2014-08-12 22:36:04 -0500

    Se cambia el mensaje de alerta

---
+++ svnBasico/src/main/webapp/src/controller/userController.js
@@ -0,0 +1,33 @@
+    App.Controller.UserController = Backbone.View.extend({
+        el: '#main',
+        initialize: function(options) {
+            this.editTemplate = _.template($('#user').html());
+            var self = this;
+            Backbone.on('user-create', function(params) {
+                self.create(params);
+            });
+            Backbone.on('user-save', function(params) {
+                self.save(params);
+            });
+            Backbone.on('user-cancel', function(params) {
+                self.cancel(params);
+            });
+        },
+        create: function() {
+            this.userModel = new App.Model.UserModel();
+            this._renderEdit();
+            $('.datepicker').datepicker();
+        },
+        save:function() { 
+            var model = $('#userForm').serializeObject();
+            document.getElementById("nuevoId").innerHTML = "El usuario " + model.firstName + " " + 
+            model.lastName + " nacio el " + model.algo;
+        },
+        cancel: function(){
+            alert('Cancel');
+        },
+        _renderEdit: function() {
+            var self = this;
+            self.$el.html(self.editTemplate({user: self.userModel}));
+        }
+    });
